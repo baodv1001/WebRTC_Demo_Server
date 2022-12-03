a9c8b875442d71e549946f170187202688d21713
quangpn24 <19522100@gm.uit.edu.vn>
2022-12-03 11:51:12 +0700

    Remove somethings

--- app.js
+++ app.js
@@ -69,10 +69,6 @@ io.sockets.on("connection", function (socket) {
   }
 });
 
-server.use("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 server.listen(process.env.PORT || 8181, () => {
   console.log("listening on *:3000");
 });
